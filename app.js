e34347aae7357f1e194c9494eaecd0b135cf88ee
Nikita Koval <69389602+Mysterion47@users.noreply.github.com>
2021-10-30 16:07:03 +0300

    Add code style optimization. Changed imports

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-const apiRoutes = require('./src/modules/routes/routes');
+const apiRoutes = require("./backend/modules/routes/routes");
 
 app.use(cors());
 
-const url = "mongodb+srv://nkovalexceed:myst0347cl98@cluster0.1pxcu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+const url =
+  "mongodb+srv://nkovalexceed:Th2UK3ntC@cluster0.1pxcu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+mongoose.connection.on("open", function (err, doc) {
+  console.log("MongoDB connected");
+});
 
 app.use(bodyParser.json());
-app.use('/', apiRoutes)
+app.use("/", apiRoutes);
 
 app.listen(8080, () => {
-  console.log('Starting...')
+  console.log("Starting...");
 });
